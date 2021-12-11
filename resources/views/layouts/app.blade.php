a14d4484705eb6151a7c16d54bb2ce2c0bb34f19
noora <noorayaqub44@gmail.com>
2021-12-11 11:33:52 -0800

    initial set-up'

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+    <title>Laravel File Upload</title>
+    <style>
+        .container {
+            max-width: 500px;
+        }
+        dl, ol, ul {
+            margin: 0;
+            padding: 0;
+            list-style: none;
+        }
+    </style>
+</head>
+
+<body>
+<body>
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+              <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+            </a>
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="/" class="nav-link px-2 text-white">File Uploader </a></li>
+              <li><a href="/search" class="nav-link px-2 text-white">Search </a></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      @yield('content')
+</body>
+</html>
---
+++ resources/views/search.blade.php
@@ -0,0 +1,18 @@
+
+    @extends('layouts.app')
+
+    @section('content')
+    <div class="container mt-5">
+
+          <h3 class="text-center mb-5">Search files Here</h3>
+
+          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+
+
+
+
+    </div>
+
+
+
+    @endsection
