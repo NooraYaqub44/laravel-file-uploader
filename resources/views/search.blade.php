e54e7d88784a05f43a1345c559ad5d6a91fb9fa6
Noora <noorayaqub44@gmail.com>
2021-12-15 12:40:52 -0800

    fixing the design

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,12 +2,13 @@
     @extends('layouts.app')
 
     @section('content')
-    <div class=" container mt-5 p-2">
+    <div class=" container mt-5">
 
           <h3 class="text-center mb-5">Search files Here</h3>
-
+          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
           <input class="form-control mr-sm-2" name="search" id="search_keyword" type="text" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0 " id="search" type="submit">Search</button>
+          <button class="btn btn-outline-success " id="search" type="submit">Search</button>
+          </div>
 
 
 
